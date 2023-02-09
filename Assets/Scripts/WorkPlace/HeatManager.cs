8c82682eb3c1286ad13455eed0d219a434b884d4
ZLKam <tonykam920@gmail.com>
2023-02-09 13:25:32 +0800

    .

--- Assets/Scripts/WorkPlace/HeatManager.cs
+++ Assets/Scripts/WorkPlace/HeatManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace HeatChecks
@@ -13,10 +14,21 @@ namespace HeatChecks
         [SerializeField] ApplianceAttribute window;
         [SerializeField] ApplianceAttribute fan;
         [SerializeField] ApplianceAttribute aircon;
+
+        private Sprite windowOpenSprite;
+        private Sprite windowCloseSprite;
+        private float yRot;
+        private Sprite airconOpenSprite;
+        private Sprite airconCloseSprite;
+        
         // Start is called before the first frame update
         void Start()
         {
+            windowOpenSprite = Resources.Load<Sprite>("windowopen");
+            windowCloseSprite = Resources.Load<Sprite>("windowdraped");
 
+            airconCloseSprite = Resources.Load<Sprite>("Air-con");
+            airconOpenSprite = Resources.Load<Sprite>("aicongif");
         }
 
         // Update is called once per frame
@@ -38,6 +50,35 @@ namespace HeatChecks
                 {
                     airconOpen = !airconOpen;
                 }
+            }
+
+            if (windowOpen)
+            {
+                window.GetComponent<SpriteRenderer>().sprite = windowOpenSprite;
+            }
+            else
+            {
+                window.GetComponent<SpriteRenderer>().sprite = windowCloseSprite;
+            }
+            if (fanOpen)
+            {
+                yRot++;
+                fan.transform.rotation = Quaternion.Euler(new Vector3(0, yRot, 0));
+            }
+            else
+            {
+                yRot = 0;
+                fan.transform.rotation = Quaternion.Euler(Vector3.zero);
+            }
+            if (airconOpen)
+            {
+                aircon.GetComponent<SpriteRenderer>().sprite = airconOpenSprite;
+                aircon.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+            else
+            {
+                aircon.GetComponent<SpriteRenderer>().sprite = airconCloseSprite;
+                aircon.GetComponent<SpriteRenderer>().color = Color.white;
             }
         }
     }
